a60f8027de57cedaf859f32630b2464bf2e8c3d7
Brenneguer <wbrenneguer07@gmail.com>
2019-01-02 12:18:15 -0200

    Fim do teste, exclusao da classe utilizada

--- src/agendamento/Sincronizacao.java
+++
@@ -1,8 +0,0 @@
-package agendamento;
-
-public class Sincronizacao {
-	public static void main(String[] args) {
-		System.out.println("esta sincronizando corretamente");
-		System.out.println("Realmente esta");
-	}
-}
