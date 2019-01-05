0874622a40f29767282ea5136bd0efc61da148d1
Brenneguer <wbrenneguer07@gmail.com>
2019-01-05 21:40:45 -0200

    Edicção e construção da consulta visita

--- src/agendamento/dao/ConsultaDao.java
+++ src/agendamento/dao/ConsultaDao.java
@@ -11,35 +11,37 @@ import java.util.List;
 
 import agendamento.VisitaTecnica;
 
-public final class ConsultaDao {
+public class ConsultaDao {
 	Connection conn = null;
 	PreparedStatement query = null;
 	ResultSet rs = null;
 	String sql = "";
-	List<VisitaTecnica> listaVisita;
+	List<VisitaTecnica> listaVisita = null;
+
 	public List<VisitaTecnica> consultaPorData(LocalDate dataInicioSelect, LocalDate dataFimSelect) {
 		conn = Connector.abrirConexao();
 		sql = "SELECT * FROM visita_tecnica WHERE data_inicio BETWEEN (?) AND (?)";
 		VisitaTecnica v = null;
 		listaVisita = new ArrayList<>();
-		
+
 		try {
 			query = conn.prepareStatement(sql);
 			query.setString(1, dataInicioSelect.toString());
 			query.setString(2, dataFimSelect.toString());
 			rs = query.executeQuery();
-			
+
 			while (rs.next()) {
 				v = new VisitaTecnica();
 				v.setIdVisitaTecnica(rs.getInt("id_visita_tecnica"));
 				v.setNumeroChamado(rs.getInt("numero_chamado"));
-				v.setDataInicio(LocalDate.parse(rs.getString("data_inicio"), DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+				v.setDataInicio(
+						LocalDate.parse(rs.getString("data_inicio"), DateTimeFormatter.ofPattern("yyyy-MM-dd")));
 				v.setDataFim(LocalDate.parse(rs.getString("data_fim"), DateTimeFormatter.ofPattern("yyyy-MM-dd")));
 				v.setIdEmpresa(rs.getInt("id_empresa"));
 				v.setTecnico(rs.getString("tecnico"));
 				v.setLad(rs.getBoolean("is_lad"));
 				v.setSituacao(rs.getString("situacao"));
-				
+
 				listaVisita.add(v);
 			}
 		} catch (SQLException e) {
@@ -50,24 +52,68 @@ public final class ConsultaDao {
 		return listaVisita;
 	}
 	
+	public List<VisitaTecnica> consultaPorChamado(int numeroChamado) {
+		listaVisita = new ArrayList<>();
+		conn = Connector.abrirConexao();
+		sql = "SELECT * FROM visita_tecnica where numero_chamado = "+"?";
+		VisitaTecnica v = null;
+		
+		try {
+			query = conn.prepareStatement(sql);
+			query.setInt(1, numeroChamado);
+			rs = query.executeQuery();
+			
+			while (rs.next()) {
+				v = new VisitaTecnica();
+				v.setDataFim(LocalDate.parse(rs.getString("data_fim"), DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+				v.setDataInicio(LocalDate.parse(rs.getString("data_inicio"), DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+				v.setIdVisitaTecnica(rs.getInt("id_visita_tecnica"));
+				v.setNumeroChamado(rs.getInt("numero_chamado"));
+				v.setIdEmpresa(rs.getInt("id_empresa"));
+				v.setSituacao(rs.getString("situacao"));
+				v.setLad(rs.getBoolean("is_lad"));
+				
+				listaVisita.add(v);
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return listaVisita;
+	}
+
 	public List<VisitaTecnica> consultaCobradas(List<VisitaTecnica> lista) {
 		List<VisitaTecnica> listaCobradas = new ArrayList<>();
+		if (!lista.isEmpty()) {
 			for (VisitaTecnica v : lista) {
-				if (v.getLad() == true ) {
+				if (v.getLad() == true) {
 					listaCobradas.add(v);
 				}
 			}
+		}
 		return listaCobradas;
 	}
+
 	public List<VisitaTecnica> consultaTecnico(List<VisitaTecnica> lista, String tecnico) {
 		List<VisitaTecnica> listaVisitaTecnico = new ArrayList<VisitaTecnica>();
-		for (VisitaTecnica v : lista) {
-			if (v.getTecnico().equalsIgnoreCase(tecnico)) {
-				listaVisitaTecnico.add(v);
+		if (!lista.isEmpty()) {
+			for (VisitaTecnica v : lista) {
+				if (v.getTecnico().equalsIgnoreCase(tecnico)) {
+					listaVisitaTecnico.add(v);
+				}
 			}
 		}
 		return listaVisitaTecnico;
 	}
 	
+	public List<VisitaTecnica> consultaPorChamado(List<VisitaTecnica> l, int numeroChamado) {
+		List<VisitaTecnica> listaContentChamado = new ArrayList<>();
+		
+		for (VisitaTecnica v : l) {
+			if(v.getNumeroChamado() == numeroChamado) {
+				listaContentChamado.add(v);
+			}
+		}
+		return listaContentChamado;
+	}
 	
 }
--- src/agendamento/gui/GuiControllerConsultaVisita.java
+++ src/agendamento/gui/GuiControllerConsultaVisita.java
@@ -1,19 +1,25 @@
 package agendamento.gui;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import agendamento.VisitaTecnica;
+import agendamento.dao.ConsultaDao;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 public class GuiControllerConsultaVisita {
@@ -28,18 +34,18 @@ public class GuiControllerConsultaVisita {
 	@FXML
 	private TextField dataFim;
 	@FXML
-	private TextField tarefaPai;
-	@FXML
-	private TextField situacao;
-	@FXML
 	private CheckBox isCobrada;
-	
+	@FXML
+	private Button consulta;
+
 	@FXML
 	private TableView<VisitaTecnica> tabela;
 	@FXML
 	private TableColumn<VisitaTecnica, String> colunaTecnico;
 	@FXML
 	private TableColumn<VisitaTecnica, Integer> colunaNumeroChamado;
+	@FXML
+	private TableColumn<VisitaTecnica, Integer> colunaEmpresa;
 	@FXML
 	private TableColumn<VisitaTecnica, LocalDate> colunaDataInicio;
 	@FXML
@@ -50,10 +56,10 @@ public class GuiControllerConsultaVisita {
 	private TableColumn<VisitaTecnica, Boolean> colunaCobrada;
 	@FXML
 	private ObservableList<VisitaTecnica> observableVisita = FXCollections.observableArrayList();
-	
+
 	@FXML
 	private Button inicio;
-	
+
 	@FXML
 	public void selectInicio(ActionEvent action) {
 		Node n = (Node) action.getSource();
@@ -61,5 +67,57 @@ public class GuiControllerConsultaVisita {
 		String envio = "/agendamento/gui/GuiHome.fxml";
 		(new CriarView()).criarTela(stage, envio);
 	}
-	
+
+	@FXML
+	public void selectConsulta(ActionEvent action) {
+
+		System.out.println(pesquisar(dataInicio, dataFim, numeroChamado, tecnico, isCobrada).toString());
+		tabela.setVisible(true);
+	}
+
+	public void adicionarArrayList(List<VisitaTecnica> lista) {
+		if (!observableVisita.isEmpty()) {
+			observableVisita.clear();
+			System.out.println("não era vazia");
+		}
+		for (VisitaTecnica v : lista) {
+			observableVisita.add(v);
+
+			colunaTecnico.setCellValueFactory(new PropertyValueFactory<VisitaTecnica, String>("tecnico"));
+			colunaNumeroChamado.setCellValueFactory(new PropertyValueFactory<VisitaTecnica, Integer>("numeroChamado"));
+			colunaEmpresa.setCellValueFactory(new PropertyValueFactory<VisitaTecnica, Integer>("idEmpresa"));
+			colunaCobrada.setCellValueFactory(new PropertyValueFactory<VisitaTecnica, Boolean>("lad"));
+			colunaDataInicio.setCellValueFactory(new PropertyValueFactory<VisitaTecnica, LocalDate>("dataInicio"));
+			colunaDataFim.setCellValueFactory(new PropertyValueFactory<VisitaTecnica, LocalDate>("dataFim"));
+			colunaSituacao.setCellValueFactory(new PropertyValueFactory<VisitaTecnica, String>("situacao"));
+			tabela.setItems(observableVisita);
+		}
+	}
+
+	public List<VisitaTecnica> pesquisar(TextField dataInicio, TextField dataFim, TextField numeroChamado, TextField Tecnico,
+			CheckBox caixaSelect) {
+		ConsultaDao cd = new ConsultaDao();
+		List<VisitaTecnica> listaVisita = null;
+		if (numeroChamado.getText().equals("") && dataInicio.getText().equals("") && dataFim.getText().equals("")
+				&& Tecnico.getText().equals("")) {
+			Alert alert = new Alert(AlertType.INFORMATION,
+					"Você precisa inserir informações para consulta, insira um intervalo de datas.");
+			alert.setTitle("Consulta invalida");
+			alert.setHeaderText("Informação util.");
+			alert.show();
+		}
+		
+		if (!numeroChamado.getText().equals("") && !dataInicio.getText().equals("") && !dataFim.getText().equals("")) {
+			//return//
+			List<VisitaTecnica> temp = cd.consultaPorData(LocalDate.parse(dataInicio.getText(), DateTimeFormatter.ofPattern("dd/MM/yyyy")),
+					LocalDate.parse(dataFim.getText(), DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+			System.out.printf("Consultando por temp %s",temp.toString());
+			return cd.consultaPorChamado(temp, Integer.parseInt(numeroChamado.getText()));
+		} else if (!numeroChamado.getText().equals("")) {
+			return cd.consultaPorChamado(Integer.parseInt(numeroChamado.getText()));
+		}
+		
+		return listaVisita;
+	}
+
 }
