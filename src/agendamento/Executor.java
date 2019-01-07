15a3269b3f7544b01a3fb4f1f32b05e64984c4d0
Brenneguer <wbrenneguer07@gmail.com>
2019-01-07 14:11:22 -0200

    atualizando o nome da main

--- src/agendamento/Executor.java
+++ src/agendamento/Executor.java
@@ -8,7 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Test extends Application {
+public class Executor extends Application {
 
 	public void start(Stage stage) {
 		try {
